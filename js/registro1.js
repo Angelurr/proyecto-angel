77f371b9b393ebee0a2c7d83c67d4e396e414957
Angelurr <anvixoli@gmail.com>
2023-11-26 22:58:29 -0500

    new commit js

---
+++ js/registro1.js
@@ -0,0 +1,7 @@
+function cargar(){
+    let nombre = document.getElementById("username");
+    let contraseña = document.getElementById("password");
+    if(nombre.value=="" || contraseña.value==""){
+        alert("esta vacio")
+    }
+}
