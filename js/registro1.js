42e392cd5c2bc4cc9c1ea50a48114ec791f6fd7b
Angelurr <anvixoli@gmail.com>
2023-11-27 09:13:02 -0500

    new js login

--- js/registro1.js
+++ js/registro1.js
@@ -1,7 +1,27 @@
 function cargar(){
     let nombre = document.getElementById("username");
     let contraseña = document.getElementById("password");
+    
+   
+
+    const data = {
+        username: username.value,
+        password: password.value
+    }
+    console.log(data);
+
     if(nombre.value=="" || contraseña.value==""){
         alert("esta vacio")
     }
+
+    if(contraseña.value>=8){
+        alert("se a pasado del limite");
+    }else{
+        alert("esta en limite");
+    }
+
+    
+
+
+
 }
